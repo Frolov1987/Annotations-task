ce929e4a6d9750f3ff215f03f61f015d88b66e8e
Frolov1987 <116515964+Frolov1987@users.noreply.github.com>
2023-08-01 15:33:40 +0300

    Add a product annotation

--- src/app/ProductInfo.java
+++ src/app/ProductInfo.java
@@ -3,6 +3,7 @@ package app;
 @Product(name = "orange", quota = 12)
 public class ProductInfo {
 
+    @Product(name = "grape", quota = 11)
     public String getData() {
         return "No data...";
     }
